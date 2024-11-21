096429f93c43bae3510388a88de61e49f8f5020f
faceman <feisiman@yunkra.com>
2024-11-21 15:29:02 +0800

    调整文件路径和命名空间

--- FaceMan.Common/DynamicAPI/DynamicWebAPI/HttpMethodConfigure.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-namespace DynamicAPI.DynamicWebAPI
-{
-    public class HttpMethodConfigure
-    {
-        public string MethodKey { get; set; }
-        public List<string> MethodVal { get; set; }
-    }
-}
