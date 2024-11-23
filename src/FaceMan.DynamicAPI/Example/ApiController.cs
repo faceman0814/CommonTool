3243f22eae71df39ba0f557f804ba210805c055b
faceman <feisiman@yunkra.com>
2024-11-23 10:54:09 +0800

    提供使用用例

---
+++ src/FaceMan.DynamicAPI/Example/ApiController.cs
@@ -0,0 +1,37 @@
+﻿using FaceMan;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaceMan.Example
+{
+    /// <summary>
+    /// 控制器生成的API接口，不影响原有功能
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ApiController : Controller
+    {
+        [HttpGet]
+        public string GetData(string inputParam, string sss)
+        {
+            return "Hello World!";
+        }
+
+        [HttpPost]
+        public string CreateData(InputParam input)
+        {
+            return "Data created with input: " + input.Input;
+        }
+
+        [HttpPut]
+        public string UPDATEData(InputParam input)
+        {
+            return "Data UPDATE with input: " + input.Input;
+        }
+        [HttpDelete]
+        public string DeleteData(InputParam input)
+        {
+            return "Data Delete with input: " + input.Input;
+        }
+    }
+}
---
+++ src/FaceMan.DynamicAPI/Example/AttributeService.cs
@@ -0,0 +1,30 @@
+﻿using FaceMan.DynamicWebAPI;
+
+namespace FaceMan.Example
+{
+    /// <summary>
+    /// 用DynamicWebApi特性实现的动态API服务
+    /// </summary>
+    [DynamicWebApi]
+    public class AttributeService
+    {
+        public string GetData(InputParam inputParam)
+        {
+            return "Hello World!";
+        }
+
+        public string CreateData(InputParam input)
+        {
+            return "Data created with input: " + input.Input;
+        }
+
+        public string UPDATEData(InputParam input)
+        {
+            return "Data UPDATE with input: " + input.Input;
+        }
+        public string DeleteData(InputParam input)
+        {
+            return "Data Delete with input: " + input.Input;
+        }
+    }
+}
