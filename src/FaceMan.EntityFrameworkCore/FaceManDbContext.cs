f32828c004e1edb0ec8dd488715c509cdfa1fa91
faceman <feisiman@yunkra.com>
2024-11-25 15:17:42 +0800

    增加数据库工具类

---
+++ src/FaceMan.EntityFrameworkCore/FaceManDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+using System.Linq.Expressions;
+using System.Reflection;
+namespace FaceMan.EntityFrameworkCore
+{
+    public partial class FaceManDbContext : DbContext
+    {
+
+        public FaceManDbContext(DbContextOptions<FaceManDbContext> options)
+        {
+        }
+
+        //需要显示调用时注册
+        //public DbSet<UserRole> UserRole { get; set; }
+    }
+}
---
+++ src/FaceMan.EntityFrameworkCore/IDbContextProvider.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FaceMan.EntityFrameworkCore
+{
+    public interface IDbContextProvider
+    {
+        DbContext GetDbContext();
+    }
+}
